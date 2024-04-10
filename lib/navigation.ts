c3dec3cc5a7f16b9919948ac062e38debf589bf8
Vusal Alakbarov <vusal11010@gmail.com>
2024-04-10 22:25:25 +0400

    Removed locale prefix on default locale

--- lib/navigation.ts
+++ lib/navigation.ts
@@ -1,7 +1,7 @@
 import { createSharedPathnamesNavigation } from "next-intl/navigation";
 
 export const locales = ["az", "tr", "en"] as const;
-export const localePrefix = "always";
+export const localePrefix = "as-needed";
 
 export const { Link, redirect, usePathname, useRouter } =
   createSharedPathnamesNavigation({ locales, localePrefix });
