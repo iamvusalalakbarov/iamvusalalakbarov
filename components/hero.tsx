d13b703912d24608d63dc11add1d077b2236367e
Vusal Alakbarov <vusal11010@gmail.com>
2024-04-11 19:27:35 +0400

    Translated texts in Hero

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,23 +1,21 @@
 import Section from "./section";
 import TechStack from "./tech-stack";
+import { useTranslations } from "next-intl";
 
 export default function Hero() {
+  const t = useTranslations("Hero");
+
   return (
     <Section className="flex flex-col justify-center items-center h-[calc(100svh-64px)]">
-      <div>
-        <div className="flex flex-col justify-center items-center text-center md:text-left md:block mb-16">
-          <span className="text-lg">Welcome to my portfolio, 👋</span>
-          <p className="font-bold text-6xl">
-            I am <span className="text-blue-700">Vusal Alakbarov</span>.
-          </p>
-          <p className="max-w-2xl">
-            A passionate front-end developer focused to write clean and
-            efficient code, especially in React, based in Baku, Azerbaijan. 📍
-          </p>
-        </div>
-
-        <TechStack />
+      <div className="flex flex-col justify-center items-center text-center md:text-left md:block mb-16">
+        <span className="text-lg">{t("welcome")} 👋</span>
+        <p className="font-bold text-6xl">
+          {t("i-am")} <span className="text-blue-700">{t("name")}</span>.
+        </p>
+        <p className="max-w-2xl">{t("paragraph")} 📍</p>
       </div>
+
+      <TechStack />
     </Section>
   );
 }
