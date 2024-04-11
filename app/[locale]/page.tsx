0477d2f2e1a2c652a438a5d8900867d26e4f6604
Vusal Alakbarov <vusal11010@gmail.com>
2024-04-11 18:32:18 +0400

    Created Hero component and called in Home page

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,24 +1,9 @@
-import TechStack from "@/components/tech-stack";
+import Hero from "@/components/hero";
 
 export default function Home() {
   return (
     <main>
-      <section className="flex flex-col justify-center items-center h-[calc(100dvh-64px)]">
-        <div>
-          <div className="mb-16">
-            <span className="text-lg">Welcome to my portfolio, 👋</span>
-            <p className="font-bold text-6xl">
-              I am <span className="text-blue-700">Vusal Alakbarov</span>.
-            </p>
-            <p className="max-w-xl">
-              A passionate front-end developer focused to write clean and
-              efficient code, especially in React, based in Baku, Azerbaijan. 📍
-            </p>
-          </div>
-
-          <TechStack />
-        </div>
-      </section>
+      <Hero />
     </main>
   );
 }
---
+++ components/hero.tsx
@@ -0,0 +1,23 @@
+import Section from "./section";
+import TechStack from "./tech-stack";
+
+export default function Hero() {
+  return (
+    <Section className="flex flex-col justify-center items-center h-[calc(100dvh-64px)]">
+      <div>
+        <div className="mb-16">
+          <span className="text-lg">Welcome to my portfolio, 👋</span>
+          <p className="font-bold text-6xl">
+            I am <span className="text-blue-700">Vusal Alakbarov</span>.
+          </p>
+          <p className="max-w-xl">
+            A passionate front-end developer focused to write clean and
+            efficient code, especially in React, based in Baku, Azerbaijan. 📍
+          </p>
+        </div>
+
+        <TechStack />
+      </div>
+    </Section>
+  );
+}
