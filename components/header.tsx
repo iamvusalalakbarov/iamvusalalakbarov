bb87c5a353e8a6db777b7b15abb392cc732e96a4
Vusal Alakbarov <vusal11010@gmail.com>
2024-04-11 20:32:56 +0400

    Made Header sticky

--- components/header.tsx
+++ components/header.tsx
@@ -5,7 +5,7 @@ import LanguageToggle from "@/components/language-toggle";
 
 export default function Header() {
   return (
-    <header className="flex justify-between items-center h-16 px-2 md:px-6 lg:px-12 border-b-2 relative">
+    <header className="flex justify-between items-center h-16 px-2 md:px-6 lg:px-12 border-b-2 bg-background sticky inset-0">
       <IAmVusalAlakbarov />
       <Nav />
       <div className="flex justify-center items-center gap-x-2">
