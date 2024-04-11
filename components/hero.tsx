d764db38a729ab15cf4666830b0e5cef7709a0b6
Vusal Alakbarov <vusal11010@gmail.com>
2024-04-11 19:02:00 +0400

    Made texts centered on mobile

--- components/hero.tsx
+++ components/hero.tsx
@@ -5,12 +5,12 @@ export default function Hero() {
   return (
     <Section className="flex flex-col justify-center items-center h-[calc(100svh-64px)]">
       <div>
-        <div className="mb-16">
+        <div className="flex flex-col justify-center items-center text-center md:text-left md:block mb-16">
           <span className="text-lg">Welcome to my portfolio, 👋</span>
           <p className="font-bold text-6xl">
             I am <span className="text-blue-700">Vusal Alakbarov</span>.
           </p>
-          <p className="max-w-xl">
+          <p className="max-w-2xl">
             A passionate front-end developer focused to write clean and
             efficient code, especially in React, based in Baku, Azerbaijan. 📍
           </p>
