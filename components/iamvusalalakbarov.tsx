266aea571286e2b0c6d8bdac3fb67ddfd4422b4d
Vusal Alakbarov <vusal11010@gmail.com>
2024-04-11 12:45:05 +0400

    Prepared a component as logo

---
+++ components/iamvusalalakbarov.tsx
@@ -0,0 +1,12 @@
+export default function IAmVusalAlakbarov() {
+  return (
+    <code className="font-bold text-lg">
+      <span>{"<"}</span>
+      <span>I</span>
+      <span>Am</span>
+      <span>Vusal</span>
+      <span>Alakbarov</span>
+      <span>{"/>"}</span>
+    </code>
+  );
+}
