7a1cc1422257cfde98a44b058cdec456e65c4189
Vusal Alakbarov <vusal11010@gmail.com>
2024-04-12 14:28:47 +0400

    Called Projects on the Home page

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,9 +1,13 @@
 import Hero from "@/components/hero";
+//import Education from "@/components/education";
+import Projects from "@/components/projects";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <Projects />
+      {/* <Education /> */}
     </main>
   );
 }
