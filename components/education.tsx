20aed3cf836fd1eda3e428b03da1a7a7de449b37
Vusal Alakbarov <vusal11010@gmail.com>
2024-04-12 14:27:01 +0400

    Created Education component

---
+++ components/education.tsx
@@ -0,0 +1,11 @@
+import Section from "./section";
+import Heading from "./heading";
+
+export default function Education() {
+  return (
+    <Section className="">
+      <Heading>Education</Heading>
+      
+    </Section>
+  );
+}
