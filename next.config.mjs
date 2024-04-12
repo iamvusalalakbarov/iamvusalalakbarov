1fa86233bb4074bb916d8774211ebe18f91117d7
Vusal Alakbarov <vusal11010@gmail.com>
2024-04-12 14:28:18 +0400

    Added remote pattern for screenshot API

--- next.config.mjs
+++ next.config.mjs
@@ -13,6 +13,12 @@ const nextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "api.screenshotone.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 };
