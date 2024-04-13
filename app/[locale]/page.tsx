8d589d0d7745d91f1fbf216e4c9ccb031c57ea31
Vusal Alakbarov <vusal11010@gmail.com>
2024-04-13 15:04:10 +0400

    Called Education component in Home page

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,4 +1,5 @@
 import Hero from "@/components/hero";
+import Education from "@/components/education";
 import Projects from "@/components/projects";
 import Contact from "@/components/contact";
 
@@ -6,6 +7,7 @@ export default function Home() {
   return (
     <main>
       <Hero />
+      <Education />
       <Projects />
       <Contact />
     </main>
