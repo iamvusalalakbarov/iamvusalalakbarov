c15445b3b58af0c5ff057b2c3bc28995face6f2a
Vusal Alakbarov <vusal11010@gmail.com>
2024-04-13 15:12:21 +0400

    Converted the logo to a link to the top

--- components/iamvusalalakbarov.tsx
+++ components/iamvusalalakbarov.tsx
@@ -1,12 +1,16 @@
+import Link from "next/link";
+
 export default function IAmVusalAlakbarov() {
   return (
-    <code className="font-bold text-lg">
-      <span>{"<"}</span>
-      <span>I</span>
-      <span>Am</span>
-      <span>Vusal</span>
-      <span>Alakbarov</span>
-      <span>{"/>"}</span>
-    </code>
+    <Link href="/">
+      <code className="font-bold text-lg">
+        <span>{"<"}</span>
+        <span>I</span>
+        <span>Am</span>
+        <span>Vusal</span>
+        <span>Alakbarov</span>
+        <span>{"/>"}</span>
+      </code>
+    </Link>
   );
 }
