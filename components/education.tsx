e98da54f44300851b67ddbc8cbbda38394ed69ab
Vusal Alakbarov <vusal11010@gmail.com>
2024-04-13 15:03:47 +0400

    Implemented Education section

--- components/education.tsx
+++ components/education.tsx
@@ -1,11 +1,66 @@
 import Section from "./section";
 import Heading from "./heading";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { useTranslations } from "next-intl";
 
 export default function Education() {
+  const t = useTranslations("Education");
+
+  const studies = [
+    {
+      value: t("asoiu.value"),
+      name: t("asoiu.name"),
+      period: "09/2016 - 06/2018",
+      department: t("asoiu.department"),
+      location: t("asoiu.location"),
+      description: t("asoiu.description"),
+    },
+    {
+      value: "Akdeniz",
+      name: t("au.name"),
+      period: "09/2018 - 01/2024",
+      department: t("au.department"),
+      location: t("au.location"),
+      description: t("au.description"),
+    },
+    {
+      value: "Self-study",
+      name: t("ss.name"),
+      period: t("ss.period"),
+      description: t("ss.description"),
+    },
+  ];
+
   return (
     <Section id="education" className="">
-      <Heading>Education</Heading>
-      
+      <Heading>{t("education")}</Heading>
+      <Tabs
+        defaultValue={studies[0].value}
+        className="flex flex-col items-center md:items-start gap-y-1 max-w-[640px]"
+      >
+        <TabsList>
+          {studies.map((study, index) => (
+            <TabsTrigger value={study.value} key={index}>
+              {study.value}
+            </TabsTrigger>
+          ))}
+        </TabsList>
+        {studies.map((study, index) => (
+          <TabsContent value={study.value} key={index} className="h-60">
+            <h3 className="font-medium md:text-lg text-primary">
+              {study.name}
+            </h3>
+            {study.department && (
+              <div className="text-sm">{study.department}</div>
+            )}
+            <div className="flex justify-between items-center font-light text-xs opacity-50 mb-2">
+              <span>{study.period}</span>
+              {study.location && <span>{study.location}</span>}
+            </div>
+            <p className="text-sm">{study.description}</p>
+          </TabsContent>
+        ))}
+      </Tabs>
     </Section>
   );
 }
